86501d1d2e2529fdb73f1577f4e9c710fb6f4f2b
jiho <kjg970201@gmail.com>
2021-10-20 16:11:05 +0900

    이름표붙이기

--- App.js
+++ App.js
@@ -29,7 +29,7 @@ import {
 const App: () => Node = () => {
   return (
     <View style = {styles.container}>
-      <Text style = {styles.hello}>지금 시각은 4:04분에 테스트 하고 있습니다.</Text>
+      <Text style = {styles.hello}>저희 이제 능숙해요 </Text>
     </View>
   );
 };
