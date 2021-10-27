5ec5036bc96af64898b220b70a6234125fbb98a8
2JuMyeong <gzzjk159@gmail.com>
2021-10-27 19:34:18 +0900

    aaa

--- App.js
+++ App.js
@@ -1,48 +1,96 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
+import 'react-native-gesture-handler'
+import * as React from 'react';
+import {NavigationContainer, useNavigation } from '@react-navigation/native';
+//import { createStackNavigator } from '@react-navigation/stack';
+import{
+  Button,
+  Dimensions,
   StyleSheet,
-  Text,
-  useColorScheme,
+  SafeAreaView, 
   View,
+  Text,
+  ScrollView,
+  Image,
+  TextInput,
+  Touchable,
 } from 'react-native';
+// import _ from 'lodash';
+import styled from 'styled-components';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { assertTSExpressionWithTypeArguments } from '@babel/types';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import { TouchableHighlight } from 'react-native-gesture-handler';
+//const Stack = createStackNavigator();
+//component를 꾸밀때 쓰는 방법
+/* 
+const name = stled.(component)`
+  자바스크립트 문법
+`;
+*/
+const Row = styled.View`
+  flex-direction : row;
+`;
+const HomeContainer = styled.SafeAreaView`
+  flex : 1;
+`;
+const Search = styled.TextInput`
+  margin-left : 20;
+  margin-right : 20;
+`;
+const Contents = styled.ScrollView`
+  flex : 13;
+`;
+const Menu = styled.Button`
+  align-items : end;
+`;
+const Topvar = styled.View`
+  flex : 1;
+  flex-direction : row;
+  align-items : center;
+`;
+const Tab = styled.View`
+  flex : 1;
+`;
+//변수 선언
+//let numbers = [];
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+//_.times( 45, n => numbers.push( n + 1 ) );
+//numbers = _.shuffle( numbers );
 
-const App: () => Node = () => {
+export default function App () {
+
   return (
-    <View style = {styles.container}>
-      <Text style = {styles.hello}>지금 시각은 4:04분에 테스트 하고 있습니다.</Text>
-    </View>
-  );
+      <HomeContainer>
+          <Topvar>
+            <Text style = {styles.moaynang}>모아냥</Text>
+            <Image source = { require('./image/moanyang.png') } style={ { width : 50,height:50 }}/>
+            <Search style = { { width: 100, height: 10, borderColor : 'gray', borderWidth  :0.5}} />
+            <Menu title="" onPress = { () => {} }/>
+          </Topvar>
+          <Tab style = { styles.blue }>
+            <Text>ㅁㄴㅇ린어리</Text>
+          </Tab>
+        <Contents style = { styles.black}>
+            <Text>sdfsfsdfdsf</Text>
+        </Contents>
+      </HomeContainer>
+  )
 };
-
 const styles = StyleSheet.create({
-  container : {
-    flex : 1,
-    justifyContent : 'center',
-    alignItems : 'center',
+  moaynang : {
+    fontWeight :'Bold',
+    padding : 10,
+    fontSize : 20,
   },
-  hello : {
-    color : 'red',
-  }
-});
+ Home : {
+    backgroundColor : '#ffff00',
+ },
+ blue : {
+   backgroundColor : 'blue',
+ },
+ black : {
+  backgroundColor : 'black'
+ },
 
-export default App;
+});
