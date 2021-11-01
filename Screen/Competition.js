35d13b4af3c15882e29f8a21eb81609e14a965cf
2JuMyeong <gzzjk159@gmail.com>
2021-11-01 23:03:56 +0900

    새로운 브랜치

--- Screen/Competition.js
+++ Screen/Competition.js
@@ -27,7 +27,7 @@ export default function Competition(){
         <Container>
             <Scroll>
                 <Text>
-                    Competition
+                    asdasdasdas
                 </Text>
             </Scroll>
         </Container>
